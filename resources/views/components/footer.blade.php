e96df7e7c5d0198719dd577a0fcca927fdd80620
Liedleee <nursalilliedle@gmail.com>
2025-12-15 17:13:39 +0700

    footer, navbar, component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-green-600 text-white text-center py-8 mt-12">
+    <div class="max-w-7xl mx-auto px-6">
+        <p class="text-white">&copy; {{ date('Y') }} Desa Bomo. Semua Hak Dilindungi.</p>
+        <p class="text-green-100 text-sm mt-2">Rogojampi, Kecamatan Rogojampi, Kabupaten Banyuwangi</p>
+    </div>
+</footer>
